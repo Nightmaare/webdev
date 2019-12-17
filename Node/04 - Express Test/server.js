a9b9757fccec1c35771bec16ab4498684b4057b9
Client10@EST.LAN <Client10@EST.LAN>
2019-12-17 17:07:59 +0000

    Added nodeJS

--- Node/04 - Express Test/server.js
+++ Node/04 - Express Test/server.js
@@ -1,4 +1,14 @@
 import express from express;
 app = express();
 
+app.get('/',(req,res)=>{
+    res.end('bonjour fr express');
+})
+
+app.get('/:name',(req,res)=>){
+    var a= req.params.name;
+    req.body.name;
+    res.end('bonjour fr express');
+}
+
 app.listen(8000);
